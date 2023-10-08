4cd64414c2fd1c3908979217111414c0efd3600c
130oclock <aodan13@att.net>
2023-10-08 00:11:57 -0400

    Added style and html table

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Digital Library
-A web-based digital library that stores and tracks books and reading activity.
+A web-based digital library that tracks books and reading activity.
--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const app = express();
 const hostname = process.env.LOCALHOST;
 const port = process.env.DEV_PORT;
 
-
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
