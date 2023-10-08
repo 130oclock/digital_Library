f78631b75aa440a27bf642ac554b25c31f1106c9
130oclock <aodan13@att.net>
2023-10-08 16:33:17 -0400

    Added database connection

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 require('dotenv').config();
 const express = require('express');
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 2
+});
+async function asyncFunction() {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        // Print connection thread
+        console.log(`Connected! (id=${conn.threadId})`);
+
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) return conn.end();
+    }
+}
+
+const connection = asyncFunction();
+
 const app = express();
 
 const hostname = process.env.LOCALHOST;
@@ -8,9 +33,9 @@ const port = process.env.DEV_PORT;
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/client/index.html");
+    res.sendFile(__dirname + "/client/index.html");
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
